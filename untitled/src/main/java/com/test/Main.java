b288536ee2a3ba28d44c43fd6a2134126013ef40
Ravi Ranjan <ravijob9089@gmail.com>
2026-02-08 10:05:46 +0530

    changing for conflict check

--- untitled/src/main/java/com/test/Main.java
+++ untitled/src/main/java/com/test/Main.java
@@ -4,7 +4,7 @@ package com.test;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and Ravi!");
 
         for (int i = 1; i <= 5; i++) {//TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             System.out.println("i = " + i);
